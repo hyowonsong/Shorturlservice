f5aca0635f7343950965c821ffc22926ae79d3c1
hyowonsong <woniwoni30@gmail.com>
2024-02-18 21:53:56 +0900

    [Add] DTO 추가  1) 데이터를 원래의 URL을 전달하고( ShortenUrlCreateRequsetDto를 만들어 get 메서 만들기)  단축 URL을 클라이언트에게 응답으로 보내 주면 된다.(ShortenUrlCreateResponseDto를 만들어 get 요청)  2) ShortenUrl 도메인 객체를 DTO로 변환해주기 위해, getter는 컨트롤러에서 JSON으로 변환 추가.  3) ShortenUrlRequsetDto에는 @NotNull 추가, ShortenUrlRequsetDto는 @Valid 추가

--- src/main/java/kr/co/shorturlservice/presentation/ShortenUrlCreateResponseDto.java
+++ src/main/java/kr/co/shorturlservice/presentation/ShortenUrlCreateResponseDto.java
@@ -1,2 +1,21 @@
-package kr.co.shorturlservice.presentation;public class ShortenUrlCreateResponseDto {
+package kr.co.shorturlservice.presentation;
+
+import kr.co.shorturlservice.domain.ShortenUrl;
+
+public class ShortenUrlCreateResponseDto {
+    private String originalUrl;
+    private String shortenUrlKey;
+
+    public ShortenUrlCreateResponseDto(ShortenUrl shortenUrl){
+        this.originalUrl = shortenUrl.getOriginalUrl();
+        this.shortenUrlKey = shortenUrl.getShortenUrlKey();
+    }
+
+    public String getOriginalUrl(){
+        return originalUrl;
+    }
+
+    public String getShortenUrlKey(){
+        return shortenUrlKey;
+    }
 }
