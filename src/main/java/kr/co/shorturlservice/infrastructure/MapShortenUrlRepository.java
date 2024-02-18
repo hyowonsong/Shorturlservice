1587f85a8e8ce05608b7ee7d79ce29266e570562
hyowonsong <woniwoni30@gmail.com>
2024-02-18 21:58:12 +0900

    [Add] 단축 URL 생성 기능 추가, 단축 URL 정보 조회 기능 추가
    1. ShortenUrl을 저장하는 메서드 구현. Map으로 구현된 MapshortenUrlRepository 추가
    2. 단축 URL의 정보를 조회할 수 있는 API를 추가.
    
    1. 단축 URL 정보 조회 기능을 추가하기 위해 컨트롤러, 서비스, 레포지토리에 코드 추가.
    -> 컨트롤러에서 @PathVariable로 받은 shortenUrlKey를 사용하여 ShortenUrl을 조회하고, 다시
    ShortenUrlInformationDto로 변환하여 컨트롤러에게 반환한 후 클라이언트에게 응답을 준다.

--- src/main/java/kr/co/shorturlservice/infrastructure/MapShortenUrlRepository.java
+++ src/main/java/kr/co/shorturlservice/infrastructure/MapShortenUrlRepository.java
@@ -1,2 +1,27 @@
-package kr.co.shorturlservice.infrastructure;public class MapShortenUrlRepository {
+package kr.co.shorturlservice.infrastructure;
+
+import kr.co.shorturlservice.domain.ShortenUrl;
+import kr.co.shorturlservice.domain.ShortenUrlRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Repository
+public class MapShortenUrlRepository implements ShortenUrlRepository {
+
+    private Map<String, ShortenUrl> shortenUrls = new ConcurrentHashMap<>();
+
+    @Override
+    public void saveShortenUrl(ShortenUrl shortenUrl) {
+        shortenUrls.put(shortenUrl.getShortenUrlKey(), shortenUrl);
+    }
+
+    @Override
+    public ShortenUrl findShortenUrlByShortenUrlKey(String shortenUrlKey){
+        ShortenUrl shortenUrl = shortenUrls.get(shortenUrlKey);
+        return shortenUrl;
+    }
 }
+
--- src/main/java/kr/co/shorturlservice/presentation/ShortenUrlInformationDto.java
+++ src/main/java/kr/co/shorturlservice/presentation/ShortenUrlInformationDto.java
@@ -1,2 +1,27 @@
-package kr.co.shorturlservice.presentation;public class ShortenUrlInformationDto {
+package kr.co.shorturlservice.presentation;
+
+import kr.co.shorturlservice.domain.ShortenUrl;
+
+public class ShortenUrlInformationDto {
+    private String originalUrl;
+    private String shortenUrlKey;
+    private Long redirectCount;
+
+    public ShortenUrlInformationDto(ShortenUrl shortenUrl) {
+        this.originalUrl = shortenUrl.getOriginalUrl();
+        this.shortenUrlKey = shortenUrl.getShortenUrlKey();
+        this.redirectCount = shortenUrl.getRedirectCount();
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public String getShortenUrl() {
+        return shortenUrlKey;
+    }
+
+    public Long getRedirectCount() {
+        return redirectCount;
+    }
 }
